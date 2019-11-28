5b2b4f23e4cbabd52fc2574ace9faf612f5877c6
Pedro Sarmento <36777671+kxpedro@users.noreply.github.com>
2019-11-27 21:27:47 -0300

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,33 @@
+﻿    
+    
+    
+    open System
+
+        let Calculadora (a:double)  (b:double)  (op:string) =
+            if not (System.String.IsNullOrEmpty(op))
+            then
+                if op.Equals("+")
+                then                      
+                    printfn "Resultado: %f" (a + b)
+                elif op.Equals("-")
+                then          
+                    printfn "Resultado: %f" (a - b)
+                elif op.Equals("*")
+                then          
+                    printfn "Resultado: %f" (a * b)
+                elif op.Equals("/")          
+                then                    
+                    printfn "Resultado: %f" (a / b)
+                else
+                    printfn "Erro ao calcular."
+            else
+                printfn "Operação Invalida."
+
+        [<EntryPoint>]
+            let main argv =
+                Calculadora 2.0 2.0 "+"
+                Calculadora 2.0 2.0 "-"
+                Calculadora 2.0 2.0 "*"
+                Calculadora 2.0 2.0 "/"
+                System.Console.ReadKey() |> ignore
+                0
