7203f0bea2ee59aa776deccdaf203016df6bba5f
Pedro Sarmento <36777671+kxpedro@users.noreply.github.com>
2019-11-27 21:00:12 -0300

    Add files via upload
    Exemplo de calculo simples com C#

---
+++ Exemplo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculadora(2, 2, "+"));            
+            Console.WriteLine(Calculadora(2, 2, "-"));
+            Console.WriteLine(Calculadora(2, 2, "/"));
+            Console.WriteLine(Calculadora(2, 2, "*"));
+            Console.ReadLine();
+        }
+
+        private static string Calculadora(double value1, double value2, string op)
+        {
+            if (!string.IsNullOrEmpty(op))
+            {
+                if (op.Equals("+"))
+                    return "Resultado: " + (value1 + value2).ToString();
+                    //Resultado: 4
+                else if (op.Equals("-"))
+                    return "Resultado: " + (value1 - value2).ToString();
+                    //Resultado: 0
+                else if (op.Equals("/"))
+                    return "Resultado: " + (value1 / value2).ToString();
+                    //Resultado: 1
+                else if (op.Equals("*"))
+                    return "Resultado: " + (value1 * value2).ToString();
+                    //Resultado: 4
+                else
+                    return "Erro ao calcular.";
+            }
+            else
+                return "Operação invalida.";
+        }
+    }
+}
