18bf080e24a8f452bae6727c14959f711a913f77
Pedro Sarmento <36777671+kxpedro@users.noreply.github.com>
2019-12-04 20:22:29 -0300

    Add files via upload

---
+++ PatternMatching.fs
@@ -0,0 +1,28 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let genRate = function
+    | "potato" -> 10.00
+    | "carrot" -> 20.00    
+    | _ -> nan (* nan is a special value meaning "not a number" *)    
+
+let function1 x =
+    match x with
+    | (var1, var2) when var1 > var2 -> printfn "%d is greater than %d" var1 var2
+    | (var1, var2) when var1 < var2 -> printfn "%d is less than %d" var1 var2
+    | (var1, var2) -> printfn "%d equals %d" var1 var2
+
+[<EntryPoint>]
+let main argv =
+    function1 (1, 2)
+    function1 (2, 1)
+    function1 (3, 3)    
+    System.Console.ReadKey() |> ignore
+    //
+    printfn "\n"
+    //
+    printfn "%g" (genRate "potato")
+    printfn "%g" (genRate "carrot")
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
